eb6806c960f5e068cc06210700cb4dc9b2a97cd9
paulgibes <paulgibes@gmail.com>
2023-04-11 16:05:53 -0600

    configure webpack.config

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
   );
+
+// or
+// module.exports = function (app) {
+//   app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//   });
+// };
